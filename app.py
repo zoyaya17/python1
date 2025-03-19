5094e005b2d664a04d7551bd4952f0471912293b
zoyaya17 <zoya.ya17@gmail.com>
2025-03-19 15:47:53 +0200

    add readme, rand col, navigation template

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import random
+import os
 
 app = Flask(__name__)
 
@@ -7,71 +8,59 @@ def getRandColor():
     r = str(int(random.random() * 255))
     g = str(int(random.random() * 255))
     b = str(int(random.random() * 255))
-    rgb = 'rgb(' + r + ',' + g + ',' + b + ')'
+    rgb = f'rgb({r},{g},{b})'
     return rgb 
 
 @app.route("/", methods=['GET'])
 def great():
-
-    print(request)
-    name = request.args.get('name', '')
-    print(name)
-
+    name = request.args.get('name', 'User')
     color = getRandColor()
-
-    if name == '':
-      name = 'User'
-
-    return '<h1 style="font-size: 100px; text-align: center; color: ' + color + '">' + 'Hello, ' + str(name) + '!<br> You are running Python FLASK</h1>'
+    return render_template("navigation.html", content=f'<h1 style="font-size: 100px; text-align: center; color: {color}">Hello, {name}!<br> You are running Python FLASK</h1>')
 
 @app.route("/sum/", methods=['GET'])
 def sum():
-    print(request)
-    a = float(request.args.get('a', ''))
-    b = float(request.args.get('b', ''))
-    print(a, b)
-
-    sum = a + b
-
+    a = float(request.args.get('a', 0))
+    b = float(request.args.get('b', 0))
+    result = a + b
     color = getRandColor()
-
-    return '<h1 style="font-size: 100px; text-align: center; color: ' + color + '">' + 'SUM a & b = ' + str(sum) + '</h1>'
+    return render_template("navigation.html", content=f'<h1 style="font-size: 100px; text-align: center; color: {color}">SUM a & b = {result}</h1>')
 
 @app.route("/sub/", methods=['GET'])
 def sub():
-    print(request)
-    a = float(request.args.get('a', ''))
-    b = float(request.args.get('b', ''))
-    print(a, b)
-
-    sub = a - b
-
+    a = float(request.args.get('a', 0))
+    b = float(request.args.get('b', 0))
+    result = a - b
     color = getRandColor()
-
-    return '<h1 style="font-size: 100px; text-align: center; color: ' + color + '">' + 'SUBTRACTION a & b = ' + str(sub) + '</h1>'
+    return render_template("navigation.html", content=f'<h1 style="font-size: 100px; text-align: center; color: {color}">SUBTRACTION a & b = {result}</h1>')
 
 @app.route("/mul/", methods=['GET'])
 def mul():
-    print(request)
-    a = float(request.args.get('a', ''))
-    b = float(request.args.get('b', ''))
-    print(a, b)
-
-    mul = a * b
-
+    a = float(request.args.get('a', 0))
+    b = float(request.args.get('b', 0))
+    result = a * b
     color = getRandColor()
-
-    return '<h1 style="font-size: 100px; text-align: center; color: ' + color + '">' + 'MULTIPLICATION a & b = ' + str(mul) + '</h1>'
+    return render_template("navigation.html", content=f'<h1 style="font-size: 100px; text-align: center; color: {color}">MULTIPLICATION a & b = {result}</h1>')
 
 @app.route("/div/", methods=['GET'])
 def div():
-    print(request)
-    a = float(request.args.get('a', ''))
-    b = float(request.args.get('b', ''))
-    print(a, b)
-
-    div = a / b
-
+    a = float(request.args.get('a', 1))
+    b = float(request.args.get('b', 1))
+    if b == 0:
+        return render_template("navigation.html", content='<h1 style="font-size: 100px; text-align: center; color: red">Error: Division by zero!</h1>')
+    result = a / b
     color = getRandColor()
+    return render_template("navigation.html", content=f'<h1 style="font-size: 100px; text-align: center; color: {color}">DIVISION a & b = {result}</h1>')
 
-    return '<h1 style="font-size: 100px; text-align: center; color: ' + color + '">' + 'DIVISION a & b = ' + str(div) + '</h1>'
+@app.route("/col/", methods=['GET'])
+def col():
+    color = getRandColor()
+    return render_template("navigation.html", content=f'<h1 style="font-size: 100px; text-align: center; color: {color}">RANDOM COLOR: {color}</h1>')
+
+@app.route("/doc/", methods=['GET'])
+def doc():
+    if os.path.exists("README.md"):
+        with open("README.md", "r", encoding="utf-8") as file:
+            content = file.read().replace("\n", "<br>")
+    else:
+        content = "<h1>README.md not found</h1>"
+    return render_template("navigation.html", content=content)
